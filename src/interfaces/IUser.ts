43f399c640e869aa3c4b9b37fff303a614e7ae05
RodrigoPimienta <capitanpimienta28@gmail.com>
2025-04-14 10:24:08 -0600

    feat(Interfaces): :sparkles: add  interfaces: IUser

---
+++ src/interfaces/IUser.ts
@@ -0,0 +1,12 @@
+import { IUserBusiness } from "./IUserBusiness"
+
+export interface IUser{
+    userId: number,
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    role: "USER" | "OWNER"
+    business? : IUserBusiness | null
+    userBusiness?: IUserBusiness
+}
